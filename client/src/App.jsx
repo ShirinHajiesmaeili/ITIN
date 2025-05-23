866a10fe557f116002b0595a30eb9eb1b646188f
Shirin <esshirin486@gmail.com>
2025-05-23 15:23:50 +0200

    home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
@@ -8,7 +7,6 @@ import LandingPage from "./pages/LandingPage";
 import Footer from "./components/Footer";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import CookieConsent from "./components/CookieConsent";
-
 import Impressum from "./pages/Impressum";
 
 const App = () => {
@@ -17,24 +15,18 @@ const App = () => {
       <div className="min-h-screen bg-gray-100 text-gray-800 flex flex-col">
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<LandingPage />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/impressum" element={<Impressum />} />
-
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/cookie-consent" element={<CookieConsent />} />
           </Routes>
         </main>
 
-        {/* Conditionally render Footer, only on pages other than LandingPage */}
-        <Routes>
-          <Route path="/" element={null} /> {/* No footer on LandingPage */}
-          <Route path="*" element={<Footer />} />{" "}
-          {/* Footer on all other pages */}
-        </Routes>
+        {window.location.pathname !== "/" && <Footer />}
       </div>
     </Router>
   );
